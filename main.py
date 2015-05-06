6ec63ebdae66f6c104f817ebcc7187790db48c55
Carl Andreas Sveinsson <carl13@ru.is>
2015-05-06 16:30:49 +0000

    First

---
+++ main.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from collections import Counter
+import itertools as it
+import re
+import urllib
+from urllib import request as req
+import json
+import csv
+import xml.etree.ElementTree as ET
+from math import radians, cos, sin, asin, sqrt, pi
+import os
+
+def main(pathToFile):
